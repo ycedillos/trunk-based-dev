13dbdd34eb4687cb7e9c97468ff406b5afffa27d
Yonathan <ycedillos@applaudostudios.com>
2021-05-24 14:03:11 -0600

    testing

--- ci-cd/scripts/environment-deployment.sh
+++ ci-cd/scripts/environment-deployment.sh
@@ -6,6 +6,10 @@
 # Production v.#.#.#
 echo "TRAVIS_TAG=${TRAVIS_TAG}"
 export TARGET_ENVIRONMENT=""
+echo "TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST}"
+echo "TRAVIS_BRANCH=${TRAVIS_BRANCH}"
+echo "TRAVIS_BUILD_ID=${TRAVIS_BUILD_ID}"
+echo "TRAVIS_TAG=${TRAVIS_TAG}"
 
 IFS='-'     # space is set as delimiter
 read -ra ADDR <<< "$TRAVIS_TAG"   # TRAVIS_TAG is read into an array as strings separated by IFS
