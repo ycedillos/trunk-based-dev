24eef47925f3f465119430b22391b4bb6f43dad2
Yonathan <ycedillos@applaudostudios.com>
2021-05-26 10:27:29 -0600

    test

--- ci-cd/scripts/test.sh
+++ ci-cd/scripts/test.sh
@@ -26,3 +26,17 @@ echo "show name"
 #git rev-parse --abbrev-ref HEAD
 export BRANCH_PRUEBAS=`git rev-parse --abbrev-ref HEAD`
 echo "BRANCH_PRUEBAS=${BRANCH_PRUEBAS}"
+
+# Return if we are not in a Pull Request
+[[ "$TRAVIS_PULL_REQUEST" = "false" ]] && return
+
+GITHUB_PR_URL=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
+GITHUB_PR_BODY=$(curl -s $GITHUB_PR_URL 2>/dev/null)
+
+if [[ $GITHUB_PR_BODY =~ \"ref\":\ *\"([a-zA-Z0-9_-]*)\" ]]; then
+  export TRAVIS_BRANCH=${BASH_REMATCH[1]}
+fi
+
+if [[ $GITHUB_PR_BODY =~ \"repo\":.*\"clone_url\":\ *\"https://github\.com/([a-zA-Z0-9_-]*/[a-zA-Z0-9_-]*)\.git.*\"base\" ]]; then
+  export TRAVIS_REPO_SLUG=${BASH_REMATCH[1]}
+fi
